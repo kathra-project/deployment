9cc47bdfcbff5ac3cc87d97f525022f11180c928
Julien Boubechtoula <julien.boubechtoula@gmail.com>
2020-03-22 10:53:31 +0100

    Revert Helm version to 2.14.3

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ export purge=0
 export debug=0
 
 # Helm to wrappe
-export helmVersion=2.15.1
+export helmVersion=2.14.3
 export helmPlateform=linux-amd64
 export helmBin=$tmp/helm-local/$helmPlateform/helm
 export tillerNs=kube-system
@@ -527,6 +527,7 @@ function restartKeycloak() {
     checkCommandAndRetry "curl --output /dev/null --silent --head --fail https://keycloak.${BASE_DOMAIN}"
     [ $? -ne 0 ] && printError "https://keycloak.${BASE_DOMAIN} is not ready" && exit 1
     printDebug "Keycloak is ready"
+    return $?
 }
 export -f restartKeycloak
 
