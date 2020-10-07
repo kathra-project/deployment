edee0d33bc15d5312cea03cbd75ff72b8745619c
Julien BOUBECHTOULA <julien.boubechtoula@gmail.com>
2020-10-07 17:10:27 +0200

    Patch resolv_host for Windows

--- terraform/terraform_modules/kathra/kathra-factory/jenkins/resolv_host.sh
+++ terraform/terraform_modules/kathra/kathra-factory/jenkins/resolv_host.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 eval "$(jq -r '@sh "host=\(.host)"')"
 
-ip=$(nslookup $host | awk '/^Address: / { print $2 }' | head -n 1)
+if [ "$OSTYPE" == "msys" ]
+then
+    powershell.exe -Command "Resolve-DnsName $host | ConvertTo-Json" > $TMP/lookup_host
+    ip=$(cat $TMP/lookup_host | jq -r '.[0] | .IP4Address')
+else 
+    ip=$(nslookup $host | awk '/^Address: / { print $2 }' | head -n 1)
+fi
 
 jq -n --arg ip "$ip" --arg host "$host" '{"ip": $ip, "host": $host}'
 exit $?
